ceedc53c904e6e1305abd6bf0ad5174a2163a6ca
ahuff1315 <ahuff1315@gmail.com>
2020-08-16 14:59:21 -0500

    move footer into component

---
+++ components/ana-footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-footer class="py-16" padless>
+      <div class="d-flex flex-column section-container py-10">
+        <h3 class="text-h3">
+          Questions?
+        </h3>
+        <p class="text-h6 font-weight-light text--secondary">
+          Do you need more info or want to see some code from a private repo?
+          Feel free to email me!
+        </p>
+        <div class="d-inline-flex">
+          <v-btn
+            class="text-lowercase"
+            max-width="300"
+            href="mailto:ahuff1315@gmail.com"
+            outlined
+          >
+            ahuff1315@gmail.com
+          </v-btn>
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                class="ml-4"
+                @click="copyToClipboard('ahuff1315@gmail.com')"
+                icon
+                v-on="on"
+              >
+                <v-icon>
+                  mdi-content-copy
+                </v-icon>
+              </v-btn>
+            </template>
+            Copy email
+          </v-tooltip>
+        </div>
+      </div>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {
+    copyToClipboard(str) {
+      const el = document.createElement('textarea');
+      el.value = str;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,63 +1,19 @@
 <template>
   <v-app dark>
     <nuxt class="main-background" />
-    <v-footer class="py-16" padless>
-      <div class="d-flex flex-column section-container py-10">
-        <h3 class="text-h3">
-          Questions?
-        </h3>
-        <p class="text-h6 font-weight-light text--secondary">
-          Do you need more info or want to see some code from a private repo?
-          Feel free to email me!
-        </p>
-        <div class="d-inline-flex">
-          <v-btn
-            class="text-lowercase"
-            max-width="300"
-            href="mailto:ahuff1315@gmail.com"
-            outlined
-          >
-            ahuff1315@gmail.com
-          </v-btn>
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                class="ml-4"
-                @click="copyToClipboard('ahuff1315@gmail.com')"
-                icon
-                v-on="on"
-              >
-                <v-icon>
-                  mdi-content-copy
-                </v-icon>
-              </v-btn>
-            </template>
-            Copy email
-          </v-tooltip>
-        </div>
-      </div>
-    </v-footer>
+    <ana-footer />
   </v-app>
 </template>
 
 <script>
+import anaFooter from '@/components/ana-footer.vue';
+
 export default {
   data() {
     return {};
   },
-  components: {},
-  methods: {
-    copyToClipboard(str) {
-      const el = document.createElement('textarea');
-      el.value = str;
-      el.setAttribute('readonly', '');
-      el.style.position = 'absolute';
-      el.style.left = '-9999px';
-      document.body.appendChild(el);
-      el.select();
-      document.execCommand('copy');
-      document.body.removeChild(el);
-    },
+  components: {
+    anaFooter,
   },
 };
 </script>
