65fbf90c5b2dde294abc8f5eff3559fc2f3bbf58
ahuff1315 <ahuff1315@gmail.com>
2020-08-16 14:25:13 -0500

    add copy to clipboard on button click

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
     <v-footer class="py-16" padless>
       <div class="d-flex flex-column section-container py-10">
         <h3 class="text-h3">
-          Have any questions?
+          Questions?
         </h3>
         <p class="text-h6 font-weight-light text--secondary">
           Do you need more info or want to see some code from a private repo?
@@ -19,11 +19,21 @@
           >
             ahuff1315@gmail.com
           </v-btn>
-          <v-btn class="ml-4" icon>
-            <v-icon>
-              mdi-content-copy
-            </v-icon>
-          </v-btn>
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                class="ml-4"
+                @click="copyToClipboard('ahuff1315@gmail.com')"
+                icon
+                v-on="on"
+              >
+                <v-icon>
+                  mdi-content-copy
+                </v-icon>
+              </v-btn>
+            </template>
+            Copy email
+          </v-tooltip>
         </div>
       </div>
     </v-footer>
@@ -36,6 +46,19 @@ export default {
     return {};
   },
   components: {},
+  methods: {
+    copyToClipboard(str) {
+      const el = document.createElement('textarea');
+      el.value = str;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    },
+  },
 };
 </script>
 
