c8fa61fac173eeb859f428661010ed7b510147ed
ahuff1315 <ahuff1315@gmail.com>
2020-08-16 14:54:02 -0500

    move common styles into theme.scss

--- pages/survey-says.vue
+++ pages/survey-says.vue
@@ -34,31 +34,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.main-container {
-  height: 100%;
-}
-p {
-  max-width: 600px;
-}
-.section-container {
-  max-width: 700px;
-  margin: auto;
-}
-.fade-out-start {
-  transition: opacity 0.2s ease-in;
-  opacity: 0 !important;
-}
-.main-opacity {
-  animation: fadein ease-in-out 0.2s;
-  opacity: 1;
-}
-@keyframes fadein {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-</style>
+<style lang="scss"></style>
