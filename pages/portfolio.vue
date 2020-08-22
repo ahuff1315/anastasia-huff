84f2c7aa8dd46574a29e9422fb8c6af97001ea7b
ahuff1315 <ahuff1315@gmail.com>
2020-08-22 12:00:30 -0500

    add portfolio site page

---
+++ pages/portfolio.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="main-container py-16">
+    <div class="main-opacity" :class="[fadeOutStart ? 'fade-out-start' : '']">
+      <v-btn class="ml-4 mt-4" fab top left fixed @click="redirect()">
+        <v-icon large>
+          mdi-chevron-left
+        </v-icon>
+      </v-btn>
+      <div class="section-container">
+        <h1 class="text-h1 pt-10 pb-6">
+          Portfolio Site
+        </h1>
+        <p class="text-h6 font-weight-light text--secondary">
+          Aug 2020 - Present
+        </p>
+        <p class="text-body-1 font-weight-light text--secondary">
+          This app allows users to create and answer surveys that other users
+          have created. These surveys can also display different visualizations
+          on the data that has been collected from users that have completed the
+          questionnaire.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fadeOutStart: false,
+    };
+  },
+  components: {},
+  methods: {
+    redirect() {
+      this.fadeOutStart = true;
+      setTimeout(() => {
+        this.$router.go(-1);
+      }, 200);
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
