b0bf0e005ddd1733d0173b7036d3b26c468044d3
ahuff1315 <ahuff1315@gmail.com>
2020-08-29 13:02:45 -0500

    add padding to bottom of page

--- pages/financially-financed.vue
+++ pages/financially-financed.vue
@@ -31,7 +31,7 @@
         <h3 class="text-h4">
           About this project
         </h3>
-        <p class="text-body-1 font-weight-light text--secondary"></p>
+        <p class="text-body-1 font-weight-light text--secondary pb-16"></p>
       </div>
     </div>
   </div>
