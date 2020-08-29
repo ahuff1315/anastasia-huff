2071777e69ee785899184ae34f4a0f90458fc778
ahuff1315 <ahuff1315@gmail.com>
2020-08-29 16:48:18 -0500

    update stepper with images

--- pages/financially-financed.vue
+++ pages/financially-financed.vue
@@ -36,26 +36,51 @@
           Images
         </h3>
         <!-- <p class="text-body-1 font-weight-light text--secondary pb-16"></p> -->
-        <v-stepper class="d-flex transparent" v-model="e1" vertical non-linear>
+        <v-stepper
+          class="d-flex transparent elevation-0 pt-10"
+          v-model="step"
+          vertical
+          non-linear
+        >
           <div class="d-inline-flex flex-column">
-            <v-stepper-step editable step="1"></v-stepper-step>
-            <v-stepper-step editable step="2"></v-stepper-step>
-            <v-stepper-step editable step="3"></v-stepper-step>
-            <v-stepper-step editable step="4"></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded transparent"
+              editable
+              step="1"
+            ></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded"
+              editable
+              step="2"
+            ></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded"
+              editable
+              step="3"
+            ></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded"
+              editable
+              step="4"
+            ></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded"
+              editable
+              step="5"
+            ></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded"
+              editable
+              step="6"
+            ></v-stepper-step>
+            <v-stepper-step
+              class="pl-4 py-2 pr-1 mb-2 rounded"
+              editable
+              step="7"
+            ></v-stepper-step>
           </div>
-          <div class="d-flex flex-column pa-6">
-            <!-- <v-stepper-content class="pa-6 ma-0 no-border" step="1"> -->
-            {{ stepperContent }}
-            <!-- </v-stepper-content> -->
-            <!-- <v-stepper-content class="pa-6 ma-0 no-border" step="2">
-              Test2
-            </v-stepper-content>
-            <v-stepper-content class="pa-6 ma-0 no-border" step="3">
-              Test3
-            </v-stepper-content>
-            <v-stepper-content class="pa-6 ma-0 no-border" step="4">
-              Test4
-            </v-stepper-content> -->
+          <div class="d-flex flex-column px-6 full-width">
+            <v-img :aspect-ratio="19 / 9" :src="imageSrc"></v-img>
           </div>
         </v-stepper>
       </div>
@@ -68,14 +93,51 @@ export default {
   data() {
     return {
       fadeOutStart: false,
-      e1: 1,
+      step: 1,
+      images: [
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598737296/portfolio/financially-financed-latest-post_glzcas.png',
+        },
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598737393/portfolio/financially-financed-posts_hlak7y.png',
+        },
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598736416/portfolio/financially-financed-ci-goal1_ns3fgf.png',
+        },
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598736423/portfolio/financially-financed-ci-goal2_jjwwcy.png',
+        },
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598736428/portfolio/financially-financed-ci-goal3_qhqtnj.png',
+        },
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598736432/portfolio/financially-financed-ci-goal4_t0ovhe.png',
+        },
+        {
+          detail: '',
+          src:
+            'https://res.cloudinary.com/dxbppu0tw/image/upload/v1598132246/portfolio/financially-financed-dark_tbbuqj.svg',
+        },
+      ],
     };
   },
   components: {},
   computed: {
-    stepperContent() {
-      console.log(this.e1);
-      return `Test ${this.e1}`;
+    imageSrc() {
+      console.log(this.step);
+      return this.images[this.step - 1].src;
     },
   },
   methods: {
