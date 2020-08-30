3290ae4a86d7812f0ea6a1197f350a2f26e536df
ahuff1315 <ahuff1315@gmail.com>
2020-08-30 15:23:34 -0500

    finish basic image carousel

--- components/image-carousel.vue
+++ components/image-carousel.vue
@@ -1,22 +1,8 @@
 <template>
   <div>
-    <v-stepper
-      class="d-flex transparent elevation-0 pt-10"
-      v-model="currentStep"
-      vertical
-      non-linear
-    >
-      <div class="d-inline-flex flex-column">
-        <v-stepper-step
-          v-for="step in steps"
-          :key="step"
-          class="pl-4 py-2 pr-1 mb-2 rounded transparent"
-          editable
-          :step="step"
-        ></v-stepper-step>
-      </div>
-      <div class="d-flex flex-column px-4 full-width">
-        <v-img :aspect-ratio="19 / 9" :key="imageSrc" :src="imageSrc">
+    <v-carousel height="auto" hide-delimiter-background :show-arrows="false">
+      <v-carousel-item v-for="(image, i) in images" :key="i" class="mb-16">
+        <v-img :aspect-ratio="19 / 9" :key="i" :src="image.src">
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular
@@ -27,10 +13,10 @@
           </template>
         </v-img>
         <p class="text-caption mb-0 pa-1">
-          {{ imageCaption }}
+          {{ image.detail }}
         </p>
-      </div>
-    </v-stepper>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
