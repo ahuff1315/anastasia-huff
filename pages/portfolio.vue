e4e80c40e8c9c6b112afe165ea09fbc4374c8f9d
ahuff1315 <ahuff1315@gmail.com>
2020-09-13 17:17:55 -0500

    fix capitalization on section headers

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -56,7 +56,7 @@
         <tech-chips :technologies="tech" />
         <p class="text-body-1 font-weight-light text--secondary pb-10"></p>
         <h4 class="text-h4 pb-4">
-          About this project
+          About This Project
         </h4>
         <p class="text-body-1 font-weight-light text--secondary pb-16">
           This portfolio site is to show some of my projects. Since my main
