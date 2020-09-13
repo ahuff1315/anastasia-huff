79acb0df44ef9749609bfd6e27831774e54214e1
ahuff1315 <ahuff1315@gmail.com>
2020-09-13 16:40:41 -0500

    update about project section

--- pages/survey-says.vue
+++ pages/survey-says.vue
@@ -40,27 +40,56 @@
           Technologies
         </h4>
         <tech-chips :technologies="tech" />
-        <p class="text-body-1 font-weight-light text--secondary pb-16"></p>
+        <p class="text-body-1 font-weight-light text--secondary pb-10"></p>
         <h4 class="text-h4 pb-4">
           About this project
         </h4>
         <p class="text-body-1 font-weight-light text--secondary">
           This app started as a semester long class project. Survey Says is an
           app that allows you to view, create, and answer polls or
-          questionnaires.
+          questionnaires. I worked mainly on polishing the UI and fixing bugs in
+          the frontend and backend. The UI was based loosely on Twitter,
+          Facebook, Amazon, and other popular sites.
         </p>
+        <h5 class="text-h5 font-weight-light pt-6 pb-4">
+          General Functionality
+        </h5>
         <p class="text-body-1 font-weight-light text--secondary">
           You can view and answer any polls that other people have created.
-          After answering the question, you can see how your answer compares to
-          other people that have answered that same question through several
-          data visualizations that appear after you cast your vote. The polls
-          you have answered previously can be revisited in the history tab.
+          After casting your vote, you can see how your answer compares to
+          others that have also completed the poll through the data
+          visualizations that appear after you cast your vote. The polls you
+          have answered previously can be revisited in the history tab.
         </p>
-        <p class="text-body-1 font-weight-light text--secondary pb-16">
+        <p class="text-body-1 font-weight-light text--secondary">
           You also have the ability to create your own polls that other users
           can answer. A new poll requires a question and a minimum of two
-          possible choices but more choices can be added during creation.
+          possible choices but more choices can be added during creation. After
+          you finish creating your poll, you can view it on the home page.
         </p>
+        <p class="text-body-1 font-weight-light text--secondary">
+          If you have a specific question in mind, you can search for it in the
+          tab with the search icon. Polls containing the query will be displayed
+          below the search bar.
+        </p>
+        <h5 class="text-h5 font-weight-light pt-6 pb-4">
+          High Level Overview
+        </h5>
+        <p class="text-body-1 font-weight-light text--secondary">
+          We broke the app into three parts:
+        </p>
+        <ul class="text-body-1 font-weight-light text--secondary pb-16">
+          <li>
+            The backend, which was written in Python using Django.
+          </li>
+          <li>
+            The frontend, which was written in JavaScript using Angular4.
+          </li>
+          <li>
+            And a very basic data science part to rank the polls and create
+            random questions.
+          </li>
+        </ul>
       </div>
     </div>
   </div>
